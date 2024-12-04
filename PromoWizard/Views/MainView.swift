a195864bab4e7bb2bcfdf24875633f5ffa4ef61d
Jan Armbrust <jan.armbrust@googlemail.com>
2024-12-04 16:44:06 +0100

    Add view for promo codes

---
+++ PromoWizard/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  PromoWizard
+//
+//  Created by Jan Armbrust on 04.12.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        PromoCodesView()
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ PromoWizard/Views/PromoCodesView.swift
@@ -0,0 +1,18 @@
+//
+//  PromoCodesView.swift
+//  PromoWizard
+//
+//  Created by Jan Armbrust on 04.12.2024.
+//
+
+import SwiftUI
+
+struct PromoCodesView: View {
+    var body: some View {
+        Text("Promo Codes View")
+    }
+}
+
+#Preview {
+    PromoCodesView()
+}
