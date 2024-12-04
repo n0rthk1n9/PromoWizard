712cbdf78a60f79bb5dc2cba743f46dd490c9ed5
Jan Armbrust <jan.armbrust@googlemail.com>
2024-12-04 16:40:32 +0100

    Initial commit

---
+++ PromoWizard/PromoWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromoWizardApp.swift
+//  PromoWizard
+//
+//  Created by Jan Armbrust on 04.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct PromoWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
